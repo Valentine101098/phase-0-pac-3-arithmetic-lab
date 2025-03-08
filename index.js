a2e2497b5531bf6a84f26316a2cd1fc8344fd306
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-03-08 10:13:50 +0300

    used arithmetic operators to write math functions in javascript

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    // let a=1500
+    // let b=39
+return a + b;
+}
+function subtract(a, b) {
+return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+
+}
+function divide(a, b) {
+    return a / b;
+
+}
+function increment(n) {
+    return ++n
+}
+function decrement(n) {
+    return --n
+}
+
+
+function makeInt(n) {
+    return parseInt(n)
+}
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
